1d8e25aa22d4b0c8d7e16b14bf2553951cecf384
arfahhshd <120293347+arfahhshd@users.noreply.github.com>
2024-01-13 16:35:10 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ import plotly.express as px
 from PIL import Image
 
 st.set_page_config(page_title='PopStatExplore')
-st.header('Lets Explore!!')
+st.header('Lets Explore Statistic Data!')
 #st.subheader('This data include from 2010 until 2019. You can fill up the form to see the data related to you!')
 st.write('This data include from 2010 until 2019. You can fill up the form to see the data related to you!')
 
@@ -35,13 +35,11 @@ data = load_data()
 #st.write("""### We need some information to predict the population""" )
 
 sex= (
-        "Sex",
         "Male",
         "Female"
     )
 
 age =(
-        "Age",
         "0 to 4", 
         "5 to 9", 
         "10 to 14", 
@@ -61,32 +59,84 @@ age =(
         "80+"
     )
 ethnic=(
-        "Ethnics",
         "Bumiputera",
         "Chinese",
         "Indians",
         "Others"
 )
+population_column = "Population"
+
 #citizen=(
     #"Malaysian citizens",
     #"Non-Malaysian citizens")
 
-sex = st.selectbox("What is your gender?", sex)
-age = st.selectbox("What is your Age?", age)
-ethnic = st.selectbox("What is your Ethnic?", ethnic)
+sex = st.selectbox("What is your gender?", options=sex, index=None)
+age = st.selectbox("What is your Age?", options=age, index=None)
+ethnic = st.selectbox("What is your Ethnic?", options=ethnic, index=None)
 #citizen= st.selectbox ("Are you a Malaysian citizen?", citizen)
-year = st.slider("Years",2010, 2019, 2015)
+year = st.slider("Years",2010, 2019, 2011)
 
 
 ok = st.button("Let's go")
 
 if ok:
-    filtered_data = data[(data['Sex'] == sex) & (data['Age Group'] == age) & (data['Ethnic'] == ethnic) & (data['Year'] == year)] # (data['Citizen category'] == citizen)
+    filtered_data = data[(data['Sex'] == sex) & (data['Age Group'] == age) & (data['Ethnic'] == ethnic) & (data['Year'] == year) ] # (data['Citizen category'] == citizen)
     if not filtered_data.empty: 
-        st.title('Data for Selected Criteria')
-        st.dataframe(filtered_data)
+        #st.title('Data for Selected Criteria')
+        #.dataframe(filtered_data)
+        #population_values = filtered_data['Population ("000)']
+        #population_values = filtered_data['Population ("000)'].tolist()
+        #population_str = ', '.join(map(str, population_values))
+        #st.subheader("Population values:")
+        #st.subheader(population_str, ("thousand"))
+        #st.write("Population values:")
+        #st.subheader(population_values)
+        
+        # Display an image (replace 'path/to/your/image.jpg' with the actual path or URL of your image)
+        st.image("people4.png", caption="Icon by nangicon", width=180)
+        st.markdown(f"<div style='text-align: center;'>{st.image}</div>", unsafe_allow_html=True)
+    
+         # Extract and display the 'Population' column values without showing the number of rows
+        population_values = filtered_data['Population ("000)'].tolist()
+        population_str = ', '.join(map(lambda x: f"{x} thousand", population_values))
+        #st.subheader("Population values:")
+        st.subheader(population_str)
+        st.write("Total Population of selected choice")
+       
+
+        
     else:
         st.warning('No data found for the selected criteria. Please adjust your inputs.')
+
+     
+
+ # Extract and display the 'Population' column from the filtered data
+       # population_values = filtered_data['Population']
+        #st.write("Population values:")
+        #st.write(population_values)
+# Display filtered data
+#st.subheader("Filtered Data:")
+#if not filtered_data.empty:
+    # Display the relevant information from the filtered DataFrame
+    #st.write("Filtered Population Data:")
+    #for index, row in filtered_data.iterrows():
+        #st.write(f"Row {index + 1}:")
+
+# Display filtered data
+#st.subheader("Filtered Data:")
+#if not filtered_data.empty:
+    # Customize the display based on your dataset columns
+    # index, row in filtered_data.iterrows():
+       #st.write(f"Entry {index + 1}")
+    #.write(f"Population : {row['Population']}")
+        #st.write(f"Age Group: {row['Age Group']}")
+        #st.write(f"Ethnic: {row['Ethnic']}")
+        #st.write(f"Year: {row['Year']}")
+        # Add more fields as needed
+
+    # Calculate and display the total population for the filtered rows
+    #total_population = filtered_data['Population("000)']
+    #st.write(f"Total Population: {total_population}")
 
  # Display Filtered Data in a Styled Box
    # st.subheader('Data Related to Your Form:')
